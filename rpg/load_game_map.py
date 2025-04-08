33ba3f83f62a4c478d078430ec665aaf450459f7
diegofrancisco-moren <diegofrancisco.moren@edu.uah.es>
2025-04-08 02:19:14 +0200

    Unión del progreso hecho por Rubén y Diego
    Tarea de Rubén: Diseñar el primer nivel (Bosque)
    Progreso de la tarea: COMPLETADA
    
    Tarea de Diego: Diseño de la lógica de la interfaz de batalla
    Progreso de la tarea: Falta por hacer la lógica de batalla (hacer que el jugador pegue al enemigo), ya que aun no está completada la tarea que implementa las estadísticas del jugador y los enemigos.

--- rpg/load_game_map.py
+++ rpg/load_game_map.py
@@ -1,5 +1,5 @@
 """
-Load maps
+Load maps (edited)
 """
 import json
 import os
@@ -24,12 +24,15 @@ class GameMap:
     properties = None
     background_color = arcade.color.AMAZON
 
-
+background_music=False
+background_player=""
 def load_map(map_name):
     """
     Load a map
     """
 
+    global background_music
+    global background_player
     game_map = GameMap()
     game_map.map_layers = OrderedDict()
     game_map.light_layer = LightLayer(100, 100)
@@ -39,9 +42,11 @@ def load_map(map_name):
     layer_options = {
         "trees_blocking": {
             "use_spatial_hash": True,
+            "hit_box_algorithm": "Simple"  # Evita cálculo automático
         },
         "misc_blocking": {
             "use_spatial_hash": True,
+
         },
         "bridges": {
             "use_spatial_hash": True,
@@ -51,6 +56,8 @@ def load_map(map_name):
         },
     }
 
+
+
     # Read in the tiled map
     print(f"Loading map: {map_name}")
     my_map = arcade.tilemap.load_tilemap(
@@ -58,6 +65,12 @@ def load_map(map_name):
     )
 
     game_map.scene = arcade.Scene.from_tilemap(my_map)
+    if not background_music:
+        background_music = arcade.load_sound(":sounds:zelda-song-101soundboards.mp3")  # Asegúrate de que la ruta sea correcta
+        background_player=arcade.play_sound(background_music, looping=True, volume=0.1)
+        background_music = True
+
+
 
 
     if "characters" in my_map.object_lists:
@@ -117,8 +130,6 @@ def load_map(map_name):
 
     if "enemies" in my_map.object_lists:
 
-        print("Enemigos encontrados en el mapa:")
-        print(my_map.object_lists["enemies"])  # Ver qué se está cargando realmente
         f = open("../resources/data/enemies_dictionary.json")
         enemy_dictionary = json.load(f)
         enemy_object_list = my_map.object_lists["enemies"]
@@ -234,6 +245,13 @@ def load_map(map_name):
             game_map.scene.remove_sprite_list_by_object(sprite_list)
 
             game_map.scene["wall_list"].extend(sprite_list)
+
+    print(f"Map loaded: {map_name}")
+    print(f"Layers: {list(game_map.map_layers.keys())}")
+    print(f"Wall list sprites: {len(game_map.scene['wall_list'])}")
+
+
+
 
     return game_map
 
